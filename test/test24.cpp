43577c1373c618e560904b47d3952ad6a9c40021
lancercd <1311282756@qq.com>
2020-08-19 13:29:09 +0800

    内存超出

--- test/test24.cpp
+++ test/test24.cpp
@@ -1,5 +1,4 @@
-//牛客  [走出迷宫]
-//还没过   显示超出内存
+
 #include "iostream"
 #include "algorithm"
 #include "queue"
@@ -8,25 +7,26 @@
 using namespace std;
 
 const int MAXN = 5e2 + 5;
-int arr[MAXN][MAXN], dx[] = {1, -1, 0, 0}, dy[] = {0,  0, 1, -1},n,m;
-queue<pair<int, int> > que;
+int arr[MAXN][MAXN],vis[MAXN][MAXN];
+int dx[] = {1, -1, 0, 0}, dy[] = {0,  0, 1, -1};
+int n,m,end_x,end_y;
 
-bool dfs(int x, int y){
-    while(que.size()) que.pop();
-    arr[x][y] = -1;
-    que.push({x,y});
-    while(que.size())
+
+bool dfs(int _x, int _y){
+    queue<pair<int, int> > que;
+    que.push({_x,_y});
+    while(!que.empty())
     {
         pair<int, int> now = que.front(); que.pop();
         for(int i=0; i<4; ++i)
         {
             int xx = now.first + dx[i], yy = now.second + dy[i];
             if(xx < 0 || yy < 0 || xx >= m || yy >= n) continue;
-            if(arr[xx][yy] < 1) continue;
+            if(arr[xx][yy] == 0) continue;
+            if(vis[xx][yy] != 0) continue;
+            if(end_x == xx && end_y == yy){ return 1; }
 
-            if(arr[xx][yy] == 3){ return 1; }
-            arr[xx][yy] = -1;
-            // vis[xx][yy] = 1;
+            vis[xx][xx] = vis[now.first][now.second] + 1;
             que.push({xx, yy});
         }
     }
@@ -43,10 +43,9 @@ int main() {
             for(j=0; j<m; ++j)
             {
                 cin >> ch;
-                     if(ch == 'S'){start_x = j; start_y = i; arr[i][j] = 1;}
+                     if(ch == 'S'){start_x = i; start_y = j; arr[i][j] = 1;}
                 else if(ch == '.'){arr[i][j] = 1;}
-                else if(ch == '#'){arr[i][j] = 0;}
-                else if(ch == 'E'){arr[i][j] = 3;}
+                else if(ch == 'E'){arr[i][j] = 1; end_x = i;end_y = j;}
             }
         }
         dfs(start_x, start_y)? cout << "yes" << endl: cout << "No" << endl;
--- test/test25.cpp
+++ test/test25.cpp
@@ -4,31 +4,32 @@
 #include<queue>
 #include<string.h>
 using namespace std;
-const int maxlength = 505;
-int a[maxlength][maxlength];
+const int MAXN = 505;
+int a[MAXN][MAXN];
+int vis[MAXN][MAXN];
+int _dx[] = {1, -1, 0, 0}, _dy[] = {0,  0, 1, -1};
+int N, M, start_x,start_y, end_x,end_y;
 
-int selected[4][2] = { 1,0,-1,0,0,1,0,-1 };
-int N, M, S, E;
-queue<int>q;
-bool bfs(int S)
+bool bfs(int _x, int _y)
 {
-    while (!q.empty()) q.pop();
-    q.push(S);
+    queue<pair<int, int>> q;
+    q.push({_x, _y});
     while (!q.empty())
     {
-        int temp = q.front();
+        pair<int,int> temp = q.front();
         q.pop();
-        int x = temp / M ;
-        int y = temp % M;
-        for (int i = 0; i <= 3; i++)
+        int x = temp.first;
+        int y = temp.second;
+        for (int i = 0; i < 4; i++)
         {
-            int dx = x + selected[i][0];
-            int dy = y + selected[i][1];
+            int dx = x + _dx[i];
+            int dy = y + _dy[i];
             if (dx<0 || dy<0 || dx>=N || dy>=M) continue;
-            if (a[dx][dy] < 1)continue;
-            if (dx * M + dy == E) return true;
-            a[dx][dy] = 1;
-            q.push(dx * M + dy);
+            if (a[dx][dy] == 0)continue;
+            if (vis[dx][dy] != 0) continue;
+            if (dx== end_x && dy == end_y) return true;
+            vis[dx][dy] = vis[x][y] + 1;
+            q.push({dx, dy});
 
         }
     }
@@ -39,24 +40,24 @@ int main()
     while (scanf("%d%d", &N,&M) != EOF)
     {
         memset(a, 0, sizeof(a));
-        memset(dis, 0, sizeof(dis));
+        memset(vis, 0, sizeof(vis));
         for (int i = 0; i < N; i++)
             for (int j = 0; j < M; j++)
             {
                 char c;
                 scanf(" %c", &c);
                 if (c == 'S') {
-                    S = i  * M + j;
-                    a[i][j] = -1;
+                    start_x = i; start_y = j;
+                    a[i][j] = 1;
                 }
                 if (c == 'E') {
-                    E = M * i  + j;
-                    a[i][j] = 3;
+                    end_x = i;end_y = j;
+                    a[i][j] = 1;
                 }
                 if (c == '.')  a[i][j] = 1;
             }
 
-        if (bfs(S)) printf("Yes\n");
+        if (bfs(start_x, start_y)) printf("Yes\n");
         else printf("No\n");
     }
     return 0;
