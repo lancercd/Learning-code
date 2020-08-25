8128399e12c69f3bbeed279283d21411c1c3727d
lancercd <1311282756@qq.com>
2020-08-25 17:39:07 +0800

    快速幂 mod

--- test/test48.cpp
+++ test/test48.cpp
@@ -1,9 +1,25 @@
 #include "iostream"
 #include "algorithm"
+
 using namespace std;
+typedef unsigned long long ull;
 
 
+ull mypower(ull a, ull n, ull mod) {
+    ull ret = 1;
+    while (n) {
+        if (n & 1)
+            ret = ret * a % mod;
+        a = a * a % mod;
+        n >>= 1;
+    }
+    return ret;
+}
 int main() {
-    
+    int T;scanf("%d", &T);
+    while(T--){
+        ull a,b,c; scanf("%llu%llu%llu", &a, &b, &c);
+        printf("%llu\n", mypower(a,b,c));
+    }
     return 0;
 }
