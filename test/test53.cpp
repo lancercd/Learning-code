83f1fc368276880c85441f0368325c5bd4678cd3
lancercd <1311282756@qq.com>
2020-08-26 23:29:49 +0800

    全排列  剪枝   还得去学习下 next_permutation

--- test/test53.cpp
+++ test/test53.cpp
@@ -1,26 +1,29 @@
 #include "iostream"
 #include "algorithm"
-#include "vector"
+
 using namespace std;
 
-int A, B, C;
+int A, B, C, is_has = 0;
 int result[9] = {0};
 bool used[9] = {0};
-vector<vector<int> > arr;
+
 inline void dfs(int count){
+    if(count == 6){
+        int a,b;
+        a = result[0] * 100 + result[1] * 10 + result[2];
+        b = result[3] * 100 + result[4] * 10 + result[5];
+        if(b * A != B * a) return;
+    }
     if(count == 9){
 
         int a,b,c;
         a = result[0] * 100 + result[1] * 10 + result[2];
         b = result[3] * 100 + result[4] * 10 + result[5];
         c = result[6] * 100 + result[7] * 10 + result[8];
-        if(b == B * a && c == C * a)
+        if(c * A == C * a)
         {
-            vector<int> tmp;
-            tmp.push_back(a);
-            tmp.push_back(b);
-            tmp.push_back(c);
-            arr.push_back(tmp);
+            cout << a << " " << b << " " << c << endl;
+            is_has = 1;
         }
     }else{
         for(int i=0; i<9; ++i)
@@ -35,9 +38,13 @@ inline void dfs(int count){
 }
 
 int main() {
-    freopen("../data/in.txt", "r", stdin); freopen("../data/out.txt", "w", stdout);
+    // freopen("../data/in.txt", "r", stdin); freopen("../data/out.txt", "w", stdout);
     cin >> A >> B >> C;
 
     dfs(0);
+    if(!is_has){
+        cout << "No!!!" << endl;
+    }
+    // next_permutation()
     return 0;
 }
