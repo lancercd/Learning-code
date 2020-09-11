d64edc53df46d7d7eb9165d69858308dec73ce33
lancercd <1311282756@qq.com>
2020-09-11 18:30:11 +0800

    test

--- test/Test86.java
+++ test/Test86.java
@@ -1,19 +1,30 @@
 import java.util.Scanner;
-
+// java dfs
 public class Test86{
     static int N;
     static int[] arr = new int[10];
     static boolean[] used = new boolean[10];
+
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-        N = input.nextInt;
-        dfs(0, N);
+        N = input.nextInt();
+        dfs(0);
+
     }
     public static void dfs(int count){
         if(count == N){
-
+            for(int i=0; i<N; ++i){
+                System.out.print(arr[i] + " ");
+            }
+            System.out.print("\n");
             return;
         }
-
+        for(int i=0; i<N; ++i){
+            if(used[i]) continue;
+            used[i] = true;
+            arr[count] = i + 1;
+            dfs(count + 1);
+            used[i] = false;
+        }
     }
 }
---
+++ test/Test87.java
@@ -0,0 +1,7 @@
+import java.util.Scanner;
+public class Test87{
+    public static void main(String[] args){
+        char ch1 = '��';
+        System.out.println(ch1);
+    }
+}
