6e22ba0a40ace9716788d85cf5d74c7f26dc7ac9
lancercd <1311282756@qq.com>
2020-09-30 00:15:27 +0800

    test

--- test/test94.cpp
+++ test/test94.cpp
@@ -4,6 +4,23 @@
 using namespace std;
 
 int main() {
-    
+
     return 0;
 }
+
+int n = Convert.ToInt32(input.Text);
+if(n < 1)
+{
+    MessageBox.Show("输入的 N 不能小于 1");
+    return;
+}
+
+ulong sum = 0, current = 1;
+for(int i=1; i <= n; ++i)
+{
+    current *= Convert.ToUInt64(i);
+    sum += current;
+}
+
+input.Text = "";
+show.Items.Add(sum);
