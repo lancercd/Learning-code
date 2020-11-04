07ccd9885ee9368a55d7e6a61d33f7f146326b08
lancercd <1311282756@qq.com>
2020-11-04 10:07:51 +0800

    昨天测试的 使用vscode调试

--- CCPC第二场/testH_1.cpp
+++ CCPC第二场/testH_1.cpp
@@ -1,40 +1,50 @@
-#include "iostream"
 #include "algorithm"
 #include "cstdio"
+#include "iostream"
 
 using namespace std;
 
 typedef long long ll;
 
-const int MAXN=200005;
-struct node{
-  ll weight,value;
-}arr[MAXN];
+const int MAXN = 200005;
+struct node
+{
+  ll weight, value;
+} arr[MAXN];
 
-bool cmp(const node x,const node y){
+bool cmp(const node x, const node y)
+{
   return x.value * y.weight > x.weight * y.value;
 }
 
 ll dp[405];
-int N,M;
-void solve(){
-    for(int i=1; i<=N; i++) scanf("%lld%lld",&arr[i].weight, &arr[i].value);
-    sort(arr+1, arr+N+1, cmp);
-    int i;
-    ll ans=0;
-    for(i=1; i<=N && M>400; i++){
-        ans+=arr[i].value;
-        M-=arr[i].weight;
-    }
-    for(int i=0; i<=M; i++) dp[i]=0;
-    for(int id=i; id<=N; id++){
-        for(int j=M; j>=arr[id].weight; j--)
-            dp[j]=max(dp[j], dp[j - arr[id].weight] + arr[id].value);
-    }
-    printf("%lld\n", dp[M] + ans);
+int N, M;
+void solve()
+{
+  for (int i = 1; i <= N; i++)
+    scanf("%lld%lld", &arr[i].weight, &arr[i].value);
+  sort(arr + 1, arr + N + 1, cmp);
+  int i;
+  ll ans = 0;
+  for (i = 1; i <= N && M > 400; i++)
+  {
+    ans += arr[i].value;
+    M -= arr[i].weight;
+  }
+  for (int i = 0; i <= M; i++)
+    dp[i] = 0;
+  for (int id = i; id <= N; id++)
+  {
+    for (int j = M; j >= arr[id].weight; j--)
+      dp[j] = max(dp[j], dp[j - arr[id].weight] + arr[id].value);
+  }
+  printf("%lld\n", dp[M] + ans);
 }
-int main(){
-    freopen("../data/in.txt", "r", stdin); freopen("../data/out.txt", "w", stdout);
-    while(~scanf("%d%d", &N, &M))solve();
-    return 0;
+int main()
+{
+  freopen("../data/in.txt", "r", stdin);
+  freopen("../data/out.txt", "w", stdout);
+  while (~scanf("%d%d", &N, &M))
+    solve();
+  return 0;
 }
--- test/context.cpp
+++ test/context.cpp
@@ -1,12 +1,16 @@
 #include <stdio.h>
 
-int main(){
+int main()
+{
     int num;
-    double count=0.0;
+    double count = 0.0;
     scanf("%d", &num);
-    if(num <= 150) count += num*0.4463;
-    else if(num <= 400) count += 150 * 0.4463 + (num-150) * 0.4663;
-    else count += 150 * 0.4463 + 250 * 0.4663 + (num-400) * 0.5663;
+    if (num <= 150)
+        count += num * 0.4463;
+    else if (num <= 400)
+        count += 150 * 0.4463 + (num - 150) * 0.4663;
+    else
+        count += 150 * 0.4463 + 250 * 0.4663 + (num - 400) * 0.5663;
     printf("%.1lf", count);
     return 0;
 }
