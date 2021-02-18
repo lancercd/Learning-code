f778a5291b92b42ca95f0cc99fca96870d15aa72
lancercd <1311282756@qq.com>
2021-02-18 12:51:50 +0800

    单链表反转、双链表反转

--- test/test151.java
+++ test/test151.java
@@ -2,10 +2,15 @@
 public class test151{
     public static void main(String[] args)
     {
-        Node head = createNodeList();
+        // Node head = createNodeList();
+        // print(head);
+        // System.out.println("  ");
+        // print(reverseNodeList(head));
+
+        DoubleNode head = createDoubleNodeList();
         print(head);
         System.out.println("  ");
-        print(reverseNodeList(head));
+        print(reverseDoubleNodeList(head));
     }
 
 
@@ -22,11 +27,38 @@ public class test151{
         return pre;
     }
 
+
+    public static DoubleNode reverseDoubleNodeList(DoubleNode head)
+    {
+        DoubleNode pre = null, next = null;
+        while(null != head)
+        {
+            next = head.next;
+            head.next = pre;
+            head.pre = next;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+
+
     public static Node createNodeList(){
         Node head = new Node(0), tmp = head;
         for(int i=0; i<10; ++i)
         {
             tmp.next = new Node(i+1);
+            tmp = tmp.next;
+        }
+        return head;
+    }
+
+    public static DoubleNode createDoubleNodeList(){
+        DoubleNode head = new DoubleNode(0), tmp = head;
+        for(int i=0; i<10; ++i)
+        {
+            tmp.next = new DoubleNode(i + 1);
+            tmp.next.pre = tmp;
             tmp = tmp.next;
         }
         return head;
@@ -39,6 +71,22 @@ public class test151{
         {
             p(node.data);
             node = node.next;
+        }
+    }
+    public static void print(DoubleNode head)
+    {
+        DoubleNode node = head;
+        while(null != node)
+        {
+            p(node.data);
+            node = node.next;
+        }
+        System.out.println("  ");
+        node = head;
+        while(null != node && null != node.next) node = node.next;
+        while(null != node){
+            p(node.data);
+            node = node.pre;
         }
     }
     public static void p(int num)
@@ -57,6 +105,18 @@ class Node{
 
     public Node(){}
     public Node(int num){
+        this.data = num;
+    }
+}
+
+class DoubleNode{
+    public DoubleNode pre;
+    public DoubleNode next;
+    public int data;
+
+    public DoubleNode(){}
+    public DoubleNode(int num)
+    {
         this.data = num;
     }
 }
