1c39c00e8e26d8bc01ac47b89dcc17bee5cbf158
lancercd <1311282756@qq.com>
2021-02-20 16:59:28 +0800

    优化一下 leetcode 236 二叉树的最近公共祖先

--- test/test152.java
+++ test/test152.java
@@ -5,62 +5,51 @@ import java.util.Stack;
 
 public class test152 {
 
+    public TreeNode ans;
+    public test152(){this.ans = null;}
+
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(6);
-        TreeNode n2 = new TreeNode(2);
-        TreeNode n8 = new TreeNode(8);
-        TreeNode n0 = new TreeNode(0);
-        TreeNode n4 = new TreeNode(4);
-        TreeNode n7 = new TreeNode(7);
-        TreeNode n9 = new TreeNode(9);
-        TreeNode n3 = new TreeNode(3);
+        TreeNode root = new TreeNode(3);
         TreeNode n5 = new TreeNode(5);
+        TreeNode n1 = new TreeNode(1);
+        TreeNode n6 = new TreeNode(6);
+        TreeNode n2 = new TreeNode(2);
+        TreeNode n0 = new TreeNode(0);
+        TreeNode n8 = new TreeNode(8);
+        TreeNode n7 = new TreeNode(7);
+        TreeNode n4 = new TreeNode(4);
 
-        root.left = n2;
-        root.right = n8;
-        n2.left = n0;
+        root.left = n5;
+        root.right = n1;
+        n5.left = n6;
+        n5.right = n2;
+
+        n1.left = n0;
+        n1.right = n8;
+
+        n2.left = n7;
         n2.right = n4;
-
-        n8.left = n7;
-        n8.right = n9;
-
-        n4.left = n3;
-        n4.right = n5;
 
         test152 test = new test152();
 
 
-        TreeNode node = test.lowestCommonAncestor(root, n2, n4);
-        System.out.println(node);
+        TreeNode node = test.lowestCommonAncestor(root, n5, n1);
+        System.out.println(node.val);
     }
 
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        TreeNode ans = null;
-        Stack<TreeNode> p_path = new Stack<>();
-        Stack<TreeNode> q_path = new Stack<>();
-        dfs(root, p, p_path);
-        dfs(root, q, q_path);
-
-        while(p_path.size() != q_path.size()){
-            if(p_path.size() > q_path.size()) p_path.pop();
-            else q_path.pop();
-        }
-
-        while (true) {
-            ans = p_path.pop();
-            if(ans == q_path.pop()) break;
-        }
-
-        return ans;
+        dfs(root, p, q);
+        return this.ans;
     }
 
-    public boolean dfs(TreeNode node, TreeNode target, Stack<TreeNode> stack){
-        if(null == node) return false;
-        stack.push(node);
-        if(node == target) return true;
-        if(dfs(node.left, target, stack) || dfs(node.right, target, stack))return true;
-        stack.pop();
-        return false;
+    public boolean dfs(TreeNode root, TreeNode p, TreeNode q){
+        if(null == root) return false;
+        boolean lson = dfs(root.left, p, q);
+        boolean rson = dfs(root.right, p, q);
+        if((lson && rson) || ((root == p || root == q) && (lson || rson))){
+            this.ans = root;
+        }
+        return lson || rson || root == p || root == q;
     }
 
 
