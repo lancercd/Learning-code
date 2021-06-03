76cc83bb01624c0ef9a9ea5cd308e5ce93a5276e
lancercd <1311282756@qq.com>
2021-06-03 11:24:19 +0800

    类名提升测试  类名不会提升

---
+++ js/test9.js
@@ -0,0 +1,20 @@
+console.log(test1);
+console.log(test2);
+
+function test1() {
+    console.log("test1");
+}
+
+var test2 = function () {
+    console.log("test2");
+}
+
+
+
+class Persion {
+    constructor() {
+        console.log('Persion\'s constructor');
+    }
+}
+
+console.log(new Persion());
