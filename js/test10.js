943ace7eea76a8dfb3ab46b573b6712b97f08d14
lancercd <1311282756@qq.com>
2021-06-03 11:29:52 +0800

    字符串 直接声明  与 通过 new String(')  声明  大不一样

---
+++ js/test10.js
@@ -0,0 +1,16 @@
+let str1 = new String('ok');
+
+let str2 = new String('ok');
+
+let str3 = 'ok';
+let str4 = 'ok';
+
+console.log(str1 == str2);
+console.log(str1 === str2);
+
+
+console.log(str3 == str4);
+console.log(str3 === str4);
+
+console.log(str1 == str3);
+console.log(str2 === str4);
