5c59ac15bfa0c45cf20237fb6c54c135b5f15b95
lancercd <1311282756@qq.com>
2021-06-05 15:24:20 +0800

    解构赋值

---
+++ js/ES6/test3.js
@@ -0,0 +1,12 @@
+let user = {
+    name: 'lancercd',
+    age: 56,
+    number: '2018051603074'
+}
+
+const { name, number: no, age } = user;
+
+
+console.log(name);
+console.log(no);
+console.log(age);
