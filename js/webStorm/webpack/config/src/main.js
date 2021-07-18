302ba347ed24492b9ad6961498c7c439869e9f29
lancercd <1311282756@qq.com>
2021-07-18 23:48:13 +0800

    webpack初体验

---
+++ js/webStorm/webpack/config/src/main.js
@@ -0,0 +1,6 @@
+import add from './js/aaa';
+
+require('./css/normal.css');
+
+
+console.log(add(1, 3));
---
+++ js/webStorm/webpack/config/webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    mode: 'development',
+    module: {
+
+        //css loader
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            }
+        ]
+    }
+}
